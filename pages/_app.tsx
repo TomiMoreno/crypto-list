87748d386640aba92d562c5c4cfd4a3f01b49184
Tomás Canosa Moreno <tomimoreno03@gmail.com>
2022-10-01 00:19:03 -0300

    Add material UI + prettier + ESLint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
+import * as React from 'react'
 import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+import createEmotionCache from '../utils/createEmotionCache'
+import darkThemeOptions from '../styles/theme/darkThemeOptions'
+import '../styles/globals.css'
+import Head from 'next/head'
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+const clientSideEmotionCache = createEmotionCache()
+
+const lightTheme = createTheme(darkThemeOptions)
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <Head>
+          <title>Crypto List</title>
+          <meta name="description" content="Get the most trending coins!" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
